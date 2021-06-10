95270fc63108a7d73adb5b37ea83b52e6c74cdeb
Kevin Lopez <kevin.kyervnienh@gmail.com>
2021-06-10 01:51:28 -0500

    populate comment response

--- apiServices/comments/model.js
+++ apiServices/comments/model.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
-//const Town = require('../towns/model');
-//const User = require('../users/model');
+const Town = require('../towns/model');
+const User = require('../users/model');
 
 const commentSchema = new mongoose.Schema({
         name: { type : String , required: true},
